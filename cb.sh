4bfa43460312f61f1e777593708550825b9670dd
OnThinIce <764958658@qq.com>
2019-12-13 19:33:22 +0800

    first commit

---
+++ cb.sh
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+cd newapp
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd account
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd biztravel
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd booking
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd clean
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd collection
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd comment
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd directsales
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd face
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd find
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd housing
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd income
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd invoice
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd lock
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd login
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd logistics
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd main
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd order
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd photography
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd publish
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd qualification
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd realname
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd search
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd service
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd setting
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd settlement
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd student
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd tenants
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd viplodge
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
+cd ..
+cd voucher
+git checkout -b feature/rebuildDialog_zl_20191213
+git pull origin master
+git push origin feature/rebuildDialog_zl_20191213
+
